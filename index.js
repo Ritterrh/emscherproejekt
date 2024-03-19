2c41b6fc329490947b064802605d452b1c1ea8d6
Rodrigo Helwig <rodrigo.helwig@filmprojekt1.de>
2024-03-19 14:50:21 +0100

    Refactor geolocation functionality and remove unnecessary files

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
-const sqlite3 = require('sqlite3');
 const mysql2 = require('mysql2');
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+
 const port = 3000;
 
+const ordnerPfad = path.join(__dirname, 'audio');
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(express.json());
-
-
 const db = mysql2.createConnection({
-    host: 'data.filmprojekt1.de',
+    host: 'dev.filmprojekt1.de',
     port: 3306,
-    user: 'Rodi',
-    password: '1408',
-    database: 'EmscherProjekt'
+    user: 'Admin',
+    password: '',
+    database: 'test'
 })
 
 
-// API-Route zum Abrufen von Audio-Dateien im Radius
+
+app.get('/api/dateien/:dateiname', (req, res) => {
+    const dateipfad = path.join(ordnerPfad, req.params.dateiname);
+  
+    fs.access(dateipfad, fs.constants.F_OK, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(404).send('Datei nicht gefunden');
+      }
+  
+      res.setHeader('Content-Type', 'audio/mpeg');
+      res.setHeader('Content-Disposition', `attachment; filename="${req.params.dateiname}"`);
+      fs.createReadStream(dateipfad).pipe(res);
+    });
+  });
+
+
 
 app.get('/api/audio', (req, res) => {
     const userLatitude = parseFloat(req.query.userLatitude);
     const userLongitude = parseFloat(req.query.userLongitude);
     const radius = 5; // Radius in Kilometern
-
+    
     db.query('SELECT * FROM audio_files', (err, rows) => {
         if (err) {
             console.error(err.message);
-            res.status(500).json({ error: "Serverfehler" });
+           
+   
             return;
         }
 
@@ -41,11 +63,16 @@ app.get('/api/audio', (req, res) => {
     });
 });
 
+
+
+
 app.listen(port, () => {
     console.log(`Server gestartet auf Port ${port}`);
 });
 
-// Funktion zur Berechnung der Entfernung (wie zuvor definiert)
+
+
+
 function calculateDistance(lat1, lon1, lat2, lon2) {
     const R = 6371; // Erdradius in Kilometern
     const dLat = (lat2 - lat1) * (Math.PI / 180);
